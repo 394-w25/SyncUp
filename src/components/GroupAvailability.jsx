83021c2a2bc3491cc23904e47bd8ce924b3d0b16
jared2183 <jaredmyang@gmail.com>
2025-01-28 21:59:47 -0600

    removed groupID from groupavailability component

--- src/components/GroupAvailability.jsx
+++ src/components/GroupAvailability.jsx
@@ -387,7 +387,7 @@ const currentDate = `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}
 day.setDate(day.getDate() + 7);
 const nextDate = `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`;
 
-export default function GroupAvailability({ groupID, startDate, endDate, startTime, endTime}) {
+export default function GroupAvailability({ startDate, endDate, startTime, endTime}) {
   return (
     <div className="flex flex-col bg-white px-8 py-8 gap-2 rounded-[20px] shadow-[0px_7px_15.699999809265137px_0px_rgba(17,107,60,0.06)]">
       <h2 className="text-2xl mb-4">Group Availability</h2>
