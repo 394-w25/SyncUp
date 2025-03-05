98a0d902e0485ee5a71d155674311ba56850e637
dh0303 <dorahu2003@gmail.com>
2025-03-05 16:07:07 -0600

    vitest

--- src/pages/App.test.jsx
+++ src/pages/App.test.jsx
@@ -4,16 +4,9 @@ import App from './App';
 
 describe('counter tests', () => {
     
-  test("Counter should be 0 at the start", () => {
+  test("Login button", () => {
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
-  });
-
-  test("Counter should increment by one when clicked", async () => {
-    render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
+    expect(screen.getByText('Login')).toBeDefined();
   });
 
 });
