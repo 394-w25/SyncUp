c17b639da96317edb13ac9daef0d77ca27a9bb5d
dh0303 <dorahu2003@gmail.com>
2025-03-05 16:49:51 -0600

    in class testing

--- src/pages/App.test.jsx
+++ src/pages/App.test.jsx
@@ -1,12 +1,30 @@
-import {describe, expect, test} from 'vitest';
+import {describe, expect, test, vi, it} from 'vitest';
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+import MeetingPage from './MeetingPage';
+import { fetchGroupData } from "../utils/fetchGroupData";
+
+vi.mock("../utils/fetchGroupData", () => ({
+  fetchGroupData: vi.fn(),
+}));
 
 describe('counter tests', () => {
-    
-  test("Login button", () => {
-    render(<App />);
-    expect(screen.getByText('Login')).toBeDefined();
-  });
+  const mockGroupData = {
+    "createdAt": "February 6, 2025 at 11:30:51 AM UTC-6",
+    "creator": "abc",
+    "groupId": "123",
+    "participants": [],
+    "proposedDays": [],
+    "proposedEnd": 19.5,
+    "proposedStart": 8,
+    "title": "mock meeting",
+  }
+  vi.mock("src/utils/fetchGroupData.js");
+
+  it ('get user data', async() => {
+    fetchGroupData.mockReturnValue(Promise.resolve[mockGroupData]);
+    render(<MeetingPage/>);
+    expect(await screen.findByText('123')).toBeInTheDocument();
+  })
 
 });
